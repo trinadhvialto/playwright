50373e2f9f0ac2550746d3d408684ed4d53a5d7c
Saikat Bhowmick TPR <saikat.bhowmick.tpr@vialto.com>
2022-12-13 13:28:22 +0530

    Reverting to commit 6ed6a6d1f02df6642a9452eec790c8243e1f8bf8

--- Tests/Portal/playwright.config.ts
+++ Tests/Portal/playwright.config.ts
@@ -1,6 +1,6 @@
 // playwright.config.js
 // @ts-check
-const { devices } = require('@playwright/test');
+const { devices } = require('@playwright/test');    //Donot change to import module. Breaks the flow.
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
