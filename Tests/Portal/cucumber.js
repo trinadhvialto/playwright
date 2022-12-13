4e5296d3778814966f7e1911e72dc42cc7364de2
Saikat Bhowmick TPR <saikat.bhowmick.tpr@vialto.com>
2022-12-13 15:12:24 +0530

    Implementing bdd framework

---
+++ Tests/Portal/cucumber.js
@@ -0,0 +1,15 @@
+    var common = [
+        './bddframework/features/**/*.feature',
+        `--format ${
+        process.env.CI || !process.stdout.isTTY ? 'progress' : 'progress-bar'
+        }`,
+        '--parallel 20',
+        '--require-module ts-node/register',
+        '--require ./bddframework/stepDefinitions/**/*.ts',
+        '--require ./bddframework/stepDefinitions/*.ts',
+        '--require ./build/tests/support/*.ts'
+    ].join(' ');
+    
+    module.exports = {
+        default: common,
+    };
