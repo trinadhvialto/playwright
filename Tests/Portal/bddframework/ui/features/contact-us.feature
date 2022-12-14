11234ba80e3681872e1a54d316e6ddd12b6c5349
Saikat Bhowmick TPR <saikat.bhowmick.tpr@vialto.com>
2022-12-14 19:14:37 +0530

    UI BDD framework working

--- Tests/Portal/bddframework/ui/features/contact-us.feature
+++ Tests/Portal/bddframework/ui/features/contact-us.feature
@@ -1,8 +1,8 @@
-# Feature: Contact US
+Feature: Contact US
 
-#     Contact Us allows users to contact Vialto team.
+    Contact Us allows users to contact Vialto team.
 
-#     Scenario: User fills up personal details in contact us page
-#         Given User visits Vialto Page
-#         Then User navigates to contact us page
-#         Then User submits contact details
+    Scenario: User fills up personal details in contact us page
+        Given User visits Vialto Page
+        Then User navigates to contact us page
+        Then User submits contact details
--- Tests/Portal/bddframework/ui/stepdefinitions/contact-us.spec.ts
+++ Tests/Portal/bddframework/ui/stepdefinitions/contact-us.spec.ts
@@ -1,5 +1,5 @@
-// import { test, expect } from "@playwright/test";
-// import { Given, Then } from '@cucumber/cucumber';
+import { test, expect, Page } from "@playwright/test";
+import { Given, Then } from '@cucumber/cucumber';
 
 // test.describe("BDD Framework", function () {
 //     test.beforeAll(async function () {
@@ -36,3 +36,37 @@
 //         })
 //     })
 // });
+
+Given('User visits Vialto Page', async function () {
+    async (page:Page) => {
+        await page.goto("https://vialtopartners.com");
+    }
+});
+
+Then('User navigates to contact us page', async function () {
+    async (page:Page) => {
+        await page.locator('span:has-text("Solutions")').click();
+        await expect(page).toHaveURL("https://vialtopartners.com/solutions");
+    }
+});
+
+Then('User submits contact details', async function () {
+   async (page:Page) => {
+    await page.getByRole("link", { name: "Contact Us" }).click();
+    await page.getByPlaceholder("First Name").click();
+    await page.getByPlaceholder("First Name").press("CapsLock");
+    await page.getByPlaceholder("First Name").fill("D");
+    await page.getByPlaceholder("First Name").press("CapsLock");
+    await page.getByPlaceholder("First Name").fill("Demo1");
+    await page.getByPlaceholder("First Name").press("Tab");
+    await page.getByPlaceholder("Last Name").fill("demo2");
+    await page.getByPlaceholder("Last Name").press("Tab");
+    await page.getByPlaceholder("email@email.com").fill("email@gmail.com");
+    await page.getByPlaceholder("email@email.com").press("Tab");
+    await page.getByPlaceholder("Company").fill("vialtotest");
+    await page.getByPlaceholder("Company").press("Tab");
+    await page
+        .getByRole("combobox", { name: "Location" })
+        .selectOption("India");
+   }
+})
