7e9ba658d805c95a300cde850fc45fedc9bbfd3d
Saikat Bhowmick TPR <saikat.bhowmick.tpr@vialto.com>
2022-12-19 17:39:18 +0530

    Removed common-hook.ts and config.ts unused files.

--- Tests/Portal/src/tests/ui/Traveller/Login_Traveler.spec.ts
+++ Tests/Portal/src/tests/ui/Traveller/Login_Traveler.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect, Page } from '@playwright/test';
-import { TravelHomePage } from '../../../pages/travelerPages/travelerMainPage';
+import { TravelHomePage } from '../../../pages/travelerPage/travelerMainPage';
 import { LoginPage } from './../../../pages/loginPage';
 
 
