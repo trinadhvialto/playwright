4e3bed8952bc0177e4f6b436f05539e1c626eb32
Saikat Bhowmick TPR <saikat.bhowmick.tpr@vialto.com>
2023-01-05 19:59:06 +0530

    Added test cases

--- Tests/Portal/src/helpers/mfile-api-helpers.ts
+++ Tests/Portal/src/helpers/mfile-api-helpers.ts
@@ -1,3 +1,5 @@
+import { expect } from "@playwright/test";
+
 export class MFileApiHelpers {
     static async retrieveDocuments_Get(request, url, params) {
         return await request.get(url, {
@@ -5,8 +7,17 @@ export class MFileApiHelpers {
         });
     }
 
-    static retrieveDocuments_IsCorrectAPIResponseReceivedForWrongParamInput(jsonResponseReceived) {
-        let _jsonBodySuccess = jsonResponseReceived.Success[0];
-        let _jsonBodyFailure = jsonResponseReceived.Failure[0];
+    static async retrieveDocuments_IsCorrectAPIResponseReceivedForWrongParamInput(_response, _expectedResponse) {
+        
+        let _actualResponseReceived = await _response.json();
+        let _successBody = _actualResponseReceived.Success[0];
+        let _failureBody = _actualResponseReceived.Failure[0];
+
+        expect(_successBody, "Verify success body of JSON received from API call to be undefined.").toBeUndefined();
+        expect(_response.ok(), "Verify response received from API call to be false.").toBeFalsy();
+        expect(_response.status(), "Verify response status to be 500").toBe(500);
+
+        expect(_failureBody.Country, "Verify Country tag in Failure Body to be " + _expectedResponse.Country).toBe(_expectedResponse.Country);
+        expect(_failureBody.Error, "Verify Country tag in Failure Body to be " + _expectedResponse.Error).toContain(_expectedResponse.Error);
     }
 }
--- Tests/Portal/src/tests/api/MFilesAPIs/RetrieveDocumentsTests/get_MfileAPIs.spec.ts
+++ Tests/Portal/src/tests/api/MFilesAPIs/RetrieveDocumentsTests/get_MfileAPIs.spec.ts
@@ -3,80 +3,62 @@ import { JsonUtils } from "../../../../utils/json_utils";
 import { MFileApiHelpers } from "../../../../helpers/mfile-api-helpers";
 import OR from "../../../../../resources/OR.json";
 import Params from "../../../../../resources/params.json"
-import results from "../../../../../resources/response.json"
+import responses from "../../../../../resources/response.json"
 
 const url = process.env.mfileApiBaseUrl + OR.MFilesAPI.RetrieveDocuments
 
-let _response : any
+let _retrieveDocumentsParams : any
+let _actualResponse : any
 let _json : any
-let _jsonBody : any
 let _jsonBodySuccess : any
 let _jsonBodyFailure : any
+let _expectedAPIResponse : any
 
 //Retrieve Documents API Testing
-test.describe("MFiles Retrieve Documents API Tests", function () {
+test.describe("MFiles API Tests - Retrieve Documents", function () {
     
     //Pass correct params
     test("Retrieve Document API with correct params passed",async ({request}) => {
-        let retrieveDocumentsParams = Params.MFileParams.RetrieveDocuments.CorrectParams;
-        let expectedAPIResponse = results.MFileResponses.RetrieveDocuments.CorrectParams;
+        _retrieveDocumentsParams = Params.MFileParams.RetrieveDocuments.CorrectParams;
+        _expectedAPIResponse = responses.MFileResponses.RetrieveDocuments.CorrectParams;
+        _actualResponse = await MFileApiHelpers.retrieveDocuments_Get(request, url, _retrieveDocumentsParams);
 
-        _response = await MFileApiHelpers.retrieveDocuments_Get(request, url, retrieveDocumentsParams);
-
-        _json = await _response.json();
-        let actualAPIResponse = _json[0];
+        let _actualAPIResponse = await _actualResponse.json();
         
-        expect(_response.ok()).toBeTruthy();
-        expect(_response.status()).toBe(200);
-        expect(JsonUtils.areJsonFilesEqual(_json, expectedAPIResponse), "Get Response for MFile Retrieve Document API is working as expected.").toBeTruthy();
+        expect(_actualResponse.ok(), "Verify response received from API call to be true.").toBeTruthy();
+        expect(_actualResponse.status(),"Verify response status to be 200").toBe(200);
+        expect(JsonUtils.areJsonFilesEqual(_actualAPIResponse, _expectedAPIResponse), "Get Response for MFile Retrieve Document API is working as expected.").toBeTruthy();
     })
 
     //Pass Incorrect client param
     test("Retrieve Document API with Incorrect client param passed",async ({request}) => {
         let incorrectClientParam = Params.MFileParams.RetrieveDocuments.IncorrectClientID;
-        _response = await MFileApiHelpers.retrieveDocuments_Get(request, url, incorrectClientParam);
-
-        _json = await _response.json();
-        _jsonBodySuccess = _json.Success[0];
-        _jsonBodyFailure = _json.Failure[0];
-        expect(_jsonBodySuccess).toBeUndefined();
-        expect(_response.ok()).toBeFalsy();
-        expect(_response.status()).toBe(500);
-        expect(_jsonBodyFailure.Country).toBe(results.MFileResponses.RetrieveDocuments.IncorrectClientID.Country);
-        expect(_jsonBodyFailure.Error).toContain(results.MFileResponses.RetrieveDocuments.IncorrectClientID.Error);
+        _actualResponse = await MFileApiHelpers.retrieveDocuments_Get(request, url, incorrectClientParam);
+        _expectedAPIResponse = responses.MFileResponses.RetrieveDocuments.IncorrectClientID;
+        await MFileApiHelpers.retrieveDocuments_IsCorrectAPIResponseReceivedForWrongParamInput(_actualResponse, _expectedAPIResponse);
     })
 
     //Pass Incorrect engagement param
     test("Retrieve Document API with Incorrect engagement param passed",async ({request}) => {
         let incorrectEngagementParam = Params.MFileParams.RetrieveDocuments.IncorrectEngagement;
-        _response = await request.get(url, {
-            params : incorrectEngagementParam
-        });
+        _actualResponse = await MFileApiHelpers.retrieveDocuments_Get(request, url, incorrectEngagementParam);
+        _expectedAPIResponse = responses.MFileResponses.RetrieveDocuments.IncorrectEngagement;
+        await MFileApiHelpers.retrieveDocuments_IsCorrectAPIResponseReceivedForWrongParamInput(_actualResponse, _expectedAPIResponse);
+    })
 
-        _json = await _response.json();
-        _jsonBodySuccess = _json.Success[0];
-        _jsonBodyFailure = _json.Failure[0];
-        expect(_jsonBodySuccess).toBeUndefined();
-        expect(_response.ok()).toBeFalsy();
-        expect(_response.status()).toBe(500);
-        expect(_jsonBodyFailure.Country).toBe(results.MFileResponses.RetrieveDocuments.IncorrectEngagement.Country);
-        expect(_jsonBodyFailure.Error).toContain(results.MFileResponses.RetrieveDocuments.IncorrectEngagement.Error);
+    //Pass incorrect Assignee
+    test("Retrieve Document API with Incorrect assignee param passed",async ({request}) => {
+        let incorrectWorkRecordParam = Params.MFileParams.RetrieveDocuments.IncorrectAssignee;
+        _actualResponse = await MFileApiHelpers.retrieveDocuments_Get(request, url, incorrectWorkRecordParam);
+        _expectedAPIResponse = responses.MFileResponses.RetrieveDocuments.IncorrectAssignee;
+        await MFileApiHelpers.retrieveDocuments_IsCorrectAPIResponseReceivedForWrongParamInput(_actualResponse, _expectedAPIResponse);
     })
 
     //Pass Incorrect work record param
     test("Retrieve Document API with Incorrect work record param passed",async ({request}) => {
-        let incorrectWorkRecordParam = Params.MFileParams.RetrieveDocuments.IncorrectEngagement;
-        _response = await request.get(url, {
-            params : incorrectWorkRecordParam
-        });
-
-        _json = await _response.json();
-        _jsonBodySuccess = _json.Success[0];
-        _jsonBodyFailure = _json.Failure[0];
-        expect(_jsonBodySuccess).toBeUndefined();
-        expect(_response.ok()).toBeFalsy();
-        expect(_response.status()).toBe(500);
-        expect(_jsonBodyFailure.Country).toBe(results.MFileResponses.RetrieveDocuments.IncorrectWorkRecord.Country);
-        expect(_jsonBodyFailure.Error).toContain(results.MFileResponses.RetrieveDocuments.IncorrectWorkRecord.Error);
+        let incorrectWorkRecordParam = Params.MFileParams.RetrieveDocuments.IncorrectWorkRecord;
+        _actualResponse = await MFileApiHelpers.retrieveDocuments_Get(request, url, incorrectWorkRecordParam);
+        _expectedAPIResponse = responses.MFileResponses.RetrieveDocuments.IncorrectWorkRecord;
+        await MFileApiHelpers.retrieveDocuments_IsCorrectAPIResponseReceivedForWrongParamInput(_actualResponse, _expectedAPIResponse);
     })
 })
